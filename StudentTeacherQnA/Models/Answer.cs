70e49699c8d120c56398357416c69f60b2795822
shajid-hasibur <shajid.hasibur1996@gmail.com>
2024-02-06 23:03:30 +0600

    first commit

---
+++ StudentTeacherQnA/Models/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentTeacherQnA.Models
+{
+    public class Answer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
---
+++ StudentTeacherQnA/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace StudentTeacherQnA.Models
+
+{
+    
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Name { get; set; }
+        public string InstituteName { get; set; }
+        public string InstituteIDCardNumber { get; set; }
+    }
+}
